580c38d9fbba867ac949524166eebffe9b70425e
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2025-11-18 20:08:25 +0100

    (Antigravity) Setup GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/accordion-buttons-mobile/',
     server: {
         host: true // Expose to network for mobile testing if needed
     }
